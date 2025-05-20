ecf4620a4aac121d8f6d89541cb80c4dba49af02
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-20 10:03:55 +0700

    prosedur kelas stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -30,6 +30,14 @@ public:
     }
 
     int push(int value)
-    {}
+    {
+
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
 
 };
